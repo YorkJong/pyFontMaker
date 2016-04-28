f9f92bbccd1262e0dcccff4e1a837d4664cd5580
York <yukuan.jiang@gmail.com>
2016-04-28 12:25:31 +0800

    Added demo.bat

---
+++ bin/demo.bat
@@ -0,0 +1,29 @@
+@echo off
+set fontmaker=fontmaker.exe
+
+
+set outfile=filename.lst
+echo =^> Generate a filename list file (%outfile%).
+%fontmaker% name -o%outfile% char.lst
+
+set name=%outfile%
+set chars=char.lst
+set font=arial.ttf
+
+set dir=fore
+echo =^> Generate font picture of only foreground.
+%fontmaker% fore -n%name% -d%dir% -cGreen -f%font% -s40 %chars%
+
+set dir=edge
+echo =^> Generate font pictures with 1-pixel edge.
+%fontmaker% edge -n%name% -d%dir% -cRed -eGreen -f%font% -s40 %chars%
+
+set dir=shadow11
+echo =^> Generate font pictures with 1x1 shadow.
+%fontmaker% shadow11 -n%name% -d%dir% -cRed -eGreen -f%font% -s40 %chars%
+
+set dir=shadow21
+echo =^> Generate font pictures with 2x1 shadow.
+%fontmaker% shadow21 -n%name% -d%dir% -cRed -eGreen -f%font% -s40 %chars%
+
+pause
